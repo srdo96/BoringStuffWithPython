6d10c5b1b6821ef20ad9170eeb1af4bea0e05b0f
sakib <seu.sakib@gmail.com>
2020-02-08 15:40:39 +0600

    Add guess limit feature

--- guessTheNumber/guessTheNumber.py
+++ guessTheNumber/guessTheNumber.py
@@ -1,16 +1,20 @@
 import random
 
-print('I am thinking of a number between 1 and 20.')
 pcGuess = random.randint(1, 20)
-i = 1
-while True:
+print('I am thinking of a number between 1 and 20.\nGuess my number with in 3 chances')
+flag = 0
+for i in range(1,4):
 	print('Take a guess.')
 	playerGuess = int(input())
 	if pcGuess == playerGuess:
 		print('Good job! You guessed my number in ' + str(i) + ' guesses!')
+		flag = 1
 		break
 	elif pcGuess - playerGuess > 0:
 		print('Your guess is too low.')
 	else:
 		print('your guess is too high')
-	i = i + 1
+
+if flag == 0:
+	print("Sorry! Your limit is over. The number I was thinking of was " +
+	 str(pcGuess))	
